20985da49b1925e7fa9de8a3de04ce305d061332
Divit Mittal <64.69.76.69.74.m@gmail.com>
2024-01-24 14:25:59 +0530

    Major Refactoring

--- server/server_node.js
+++ server/server_node.js
@@ -1,7 +1,6 @@
 import { WebSocketServer } from "ws";
 import fs from "fs";
-import { spawn } from "child_process";
-const pythonProcess = spawn('/usr/local/bin/python3',["../recog_exec.py"]);
+import { exec } from 'child_process';
 
 const saveBase64 = (strData) => {
     fs.writeFile("./uploads/image_base64.txt", strData, (err) => {
@@ -14,21 +13,29 @@ const saveBase64 = (strData) => {
 
 const wss = new WebSocketServer({ port: 2121 });
 wss.on("connection", function connection(ws) {
+    ws.on("open", () => {console.log("Client connected!!")});
     ws.on("message", (base64String) => {
         console.log("Received base64 image");
         saveBase64(base64String);
-    });
-
-    pythonProcess.stdout.on('data', (data) => {
-        console.log(data);
-        ws.send(data.toString());
-    });
-
-    pythonProcess.on('error', (err) => {
-        console.error('Error in Python process:', err);
-    });
-
-    pythonProcess.on('close', (code) => {
-        console.log(`Python process exited with code ${code}`);
+        fs.writeFile('./output.txt', " ", (err) => {if (err) console.log(err)});
+        exec('pipenv run python3 ./recog_exec.py', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        });
+        setTimeout(() => {
+            console.log('This printed after about 1 second');
+        }, 10000);
+        fs.readFile('./output.txt', 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(data);
+              ws.send(data);
+            }
+        });
     });
 });
